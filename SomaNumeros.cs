0c864214d2896640578198efb714ea5a80dabf6f
Paulo-Rossetto <Paulozezzarossetto@gmail.com>
2024-12-13 23:57:30 -0300

    Create SomaNumeros.cs

---
+++ SomaNumeros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacoRepeticaoWhile
+{
+    internal class SomaNumeros
+    {
+        //Peça ao usuário para digitar números inteiros até que ele digite o número 0.
+        //Ao final, mostre a soma dos números digitados.
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número");
+            int somaNumero = Convert.ToInt16(Console.ReadLine());
+        }
+    }
+}
